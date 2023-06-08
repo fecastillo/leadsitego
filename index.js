de5ed30594b82b27ac9580b139815ad126bc299e
fernando castillo <ferchufer10@gmail.com>
2023-06-08 02:23:02 -0300

    reformat

--- index.js
+++ index.js
@@ -1,111 +1,117 @@
+// Importar módulos necesarios
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require("mongodb");
-const TokenDinero = process.env.TOKEN_DINERO;
-const TokenVivienda = process.env.TOKEN_VIVIENDA;
-const TokenViviendaStorino = process.env.TOKEN_STORINO;
-const TokenViviendaGral = process.env.TOKEN_VIVIENDA_GENERAL;
-const TokenAle = process.env.TOKEN_ALE;
-const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
+
+// Almacenar tokens de acceso en un objeto para facilitar su gestión
+const tokens = {
+    dinero: process.env.TOKEN_DINERO,
+    vivienda: process.env.TOKEN_VIVIENDA,
+    viviendaStorino: process.env.TOKEN_STORINO,
+    viviendaGral: process.env.TOKEN_VIVIENDA_GENERAL,
+    ale: process.env.TOKEN_ALE,
+    facebookPage: process.env.FACEBOOK_PAGE_ACCESS_TOKEN
+};
+
+// Inicializar servidor Express y definir puerto
 const app = express();
-const port = process.env.PORT || 3000;;
-var form;
-var idLead;
-// Accept JSON POST body
+const port = process.env.PORT || 3000;
+
+// Variables para almacenar datos de formulario e ID de cliente potencial
+let form;
+let idLead;
+
+// Aceptar cuerpo JSON en solicitudes POST
 app.use(bodyParser.json());
 
-// GET /webhook
+// Ruta GET /webhook
 app.get('/webhook', (req, res) => {
-    // Facebook sends a GET request
-    // To verify that the webhook is set up
-    // properly, by sending a special challenge that
-    // we need to echo back if the "verify_token" is as specified
+    // Verificar que el webhook esté configurado correctamente
     if (req.query['hub.verify_token'] === process.env.CUSTOM_WEBHOOK_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
         return;
     }
-})
+});
 
-// POST /webhook
+// Ruta POST /webhook
 app.post('/webhook', async (req, res) => {
-    // Facebook will be sending an object called "entry" for "leadgen" webhook event
-    form = req.body.entry[0].changes[0].value.form_id;
-    idLead = req.body.entry[0].changes[0].value.leadgen_id;
+    // Verificar que se haya recibido un objeto "entry" para el evento de webhook "leadgen"
     if (!req.body.entry) {
         return res.status(500).send({ error: 'Invalid POST data received' });
     }
 
-    // Travere entries & changes and process lead IDs
+    // Recorrer entradas y cambios y procesar ID de clientes potenciales
     for (const entry of req.body.entry) {
         for (const change of entry.changes) {
-            // Process new lead (leadgen_id)
-            await processNewLead(change.value.leadgen_id);
+            // Procesar nuevo cliente potencial (leadgen_id)
+            await processNewLead(change.value.leadgen_id, change.value.form_id);
         }
     }
 
-    // Success
+    // Éxito
     res.send({ success: true });
-})
+});
 
+// Iniciar servidor
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 });
 
-async function processNewLead(leadId) {
+// Función para procesar nuevo cliente potencial
+async function processNewLead(leadId, form_id) {
     let response;
-    var itego =  new Object();
-    var obj = new Object();
+    let itego = {token: '', prospecto: {}};
+    let obj = {};
 
     try {
-        // Get lead details by lead ID from Facebook API
-        response = await axios.get(`https://graph.facebook.com/v12.0/${leadId}/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`);
-    }
-    catch (err) {
-        // Log errors
+        // Obtener detalles del cliente potencial mediante ID de la API de Facebook
+        response = await axios.get(`https://graph.facebook.com/v12.0/${leadId}/?access_token=${tokens.facebookPage}`);
+       //printLead(response);
+       //console.log(response.data);
+    } catch (err) {
+        // Registrar errores
+        console.warn(`An error occurred while trying to process a new lead:`, err);
         return console.warn(`An invalid response was received from the Facebook API:`, err.response.data ? JSON.stringify(err.response.data) : err.response);
     }
 
-    // Ensure valid API response returned
+    // Verificar que se haya recibido una respuesta válida de la API
     if (!response.data || (response.data && (response.data.error || !response.data.field_data))) {
         return console.warn(`An invalid response was received from the Facebook API: ${response}`);
     }
-    // Extract fields
-    switch (form) {
-        case 665950964439281:
-            itego.token = TokenDinero;
-            break;
-        case 809830906640455:
-            itego.token = TokenVivienda;
-            break;
-        case 970738230263366:
-            itego.token = TokenViviendaStorino;
-            break;
-        case 1175746696379045:
-            itego.token = TokenViviendaGral;
-            break;
-        case 207302345223994:
-            itego.token = TokenAle;
-            break;
-        default:
-            break;
-    }
-    response.data.field_data.forEach(function(element) {obj[element.name] = element.values[0];})
-    obj.telefono = parseInt(`92${obj.telefono.substring(obj.telefono.length - 10)}`);
-    itego.prospecto = obj;
-    console.log(response.data);
-    console.log(itego);
-    sendData(itego);
 
+    // Extraer campos del formulario y almacenarlos en un objeto
+    if (form_id == 665950964439281) {
+        itego.token = tokens.dinero;
+    } else if (form_id == 809830906640455) {
+        itego.token = tokens.vivienda;
+    } else if (form_id == 970738230263366) {
+        itego.token = tokens.viviendaStorino;
+    } else if (form_id == 1175746696379045) {
+        itego.token = tokens.viviendaGral;
+    } else if (form_id == 207302345223994) {
+        itego.token = tokens.ale;
+    }
+    
+    response.data.field_data.forEach(function(element) {itego.prospecto[element.name] = element.values[0];})
+    
+    itego.prospecto.telefono = parseInt(`92${itego.prospecto.telefono.substring(itego.prospecto.telefono.length - 10)}`);
+    
+    //itego.prospecto = obj;
+    
+    //console.log(response.data);
+    
+    console.log(itego);
+    
+    sendData(itego);
 }
 
+// Función para enviar datos a una URL específica
 async function sendData(data) {
     try{
         respuesta = await axios.post('https://autocredito.itego.com.ar/index.php?r=webService/apiLanding', data);
         //console.log(respuesta.data);  
-    }
-    catch(err){
+    } catch(err) {
         console.log(err);
     }
 }
